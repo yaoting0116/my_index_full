e31e74acfb92dfeedd44f4a05cc859cda3f5a698
朝霧 <102600436+yaoting0116@users.noreply.github.com>
2025-12-29 00:49:06 +0800

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <footer className="site-footer">
-      <div>© {new Date().getFullYear()} CAI,YAO-TING Built with React</div>
-    </footer>
-  )
-}
